7867a3d43a33c209f29b86755bf225dc3030d2ed
Jean-Baptiste Lamouche <jean-baptiste.lamouche@chru-strasbourg.fr>
2023-08-16 12:48:49 +0200

    update version and date

--- vcf2circos/__init__.py
+++ vcf2circos/__init__.py
@@ -3,8 +3,8 @@ from vcf2circos.utils import launch
 __all__ = ["vcf2circos"]
 
 __author__ = "Jean-Baptiste Lamouche, Antony Le Bechec, Jin Cui"
-__version__ = "1.1"
-__date__ = "Mars 26 2023"
+__version__ = "1.1.1"
+__date__ = "16 August 2023"
 
 
 launch()
